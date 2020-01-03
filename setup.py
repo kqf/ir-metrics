0bdafcff575045d3447a51908a00cd6b660f1d1a
Sasha <q.dev.null@gmail.com>
2020-01-03 13:31:11 +0100

    Add NDCG metrics with tests

--- setup.py
+++ setup.py
@@ -4,4 +4,8 @@ setup(
     name="recometrics",
     version="0.0.1",
     packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+    ]
 )
---
+++ tests/test_ndcg.py
@@ -0,0 +1,18 @@
+import pytest
+import numpy as np
+from recometrics.ranking.ndcg import ndcg_score
+
+
+EXAMPLES = [
+    ((3, 2, 3, 0, 1, 2), 0.948810748),
+    ((1, 0, 0, 0, 0, 0), 1.0),
+    ((0, 1, 0, 0, 0, 0), 0.630929753),
+    ((1, 1, 0, 0, 0, 0), 1.0),
+    ((0, 0, 0, 0, 0, 1), 0.356207187),
+    ((0, 0, 0, 0, 0, 0), np.NaN)
+]
+
+
+@pytest.mark.parametrize("data, answer", EXAMPLES)
+def test_ndcg_score(data, answer):
+    np.testing.assert_approx_equal(ndcg_score(data), answer)
