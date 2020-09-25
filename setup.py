79caed34fe1cc943983e01dc29deec7f8510fb0b
Sasha <q.dev.null@gmail.com>
2020-09-25 21:54:31 +0300

    Add the link to the repo

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     use_scm_version=True,
     description='The most common information retrieval (IR) metrics',
     long_description=README,
+    url="https://github.com/kqf/ir-metrics",
     setup_requires=['setuptools_scm'],
     packages=find_packages(),
     install_requires=[
