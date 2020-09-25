730c46c5c28546fc343c4680d3ded7d8c903cf89
kqf <q.dev.null@gmail.com>
2020-09-25 21:32:54 +0300

    Fix the CI configuration (#3)
    * Remove the outdated code
    
    * Add the minimalistic readme
    
    * Add the long description
    
    * Re-enable test deploys
    
    * Remove the test server for good

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
+import os
 from setuptools import setup, find_packages
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    README = open(os.path.join(here, 'README.md')).read()
+except IOError:
+    README = ''
 
 setup(
     name="ir-metrics",
     use_scm_version=True,
+    description='The most common information retrieval (IR) metrics',
+    long_description=README,
     setup_requires=['setuptools_scm'],
     packages=find_packages(),
     install_requires=[
