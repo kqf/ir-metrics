e96b7697c7716cbd05395da97a500e9e24b69d03
Sasha <q.dev.null@gmail.com>
2020-12-05 10:38:08 +0200

    Check the vectorized version for exception either

--- tests/test_topk.py
+++ tests/test_topk.py
@@ -86,8 +86,10 @@ def test_all(y_true, y_pred, output, expectation, f, n_samples=128):
     with expectation():
         np.testing.assert_equal(f(y_true, y_pred), output)
 
-    # Now the vectorized output
+    # Now the vectorized version of the same function
     y_trues = np.repeat(np.array(y_true), n_samples)
     y_preds = np.repeat(np.atleast_2d(y_pred), n_samples, axis=0)
     outputs = np.repeat(np.array(output), n_samples)
-    np.testing.assert_equal(f(y_trues, y_preds), outputs)
+
+    with expectation():
+        np.testing.assert_equal(f(y_trues, y_preds), outputs)
