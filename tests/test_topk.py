cb9981bb920f4355be2937e232de5e658cca4fb6
Sasha <q.dev.null@gmail.com>
2020-12-05 10:07:32 +0200

    Move all the test data to a single data structure

--- tests/test_topk.py
+++ tests/test_topk.py
@@ -1,7 +1,67 @@
 import pytest
 import numpy as np
 
+from contextlib import contextmanager
+
 from irmetrics.topk import rr, recall, precision, ndcg, ap
+
+
+@contextmanager
+def does_not_raise():
+    yield
+
+
+INPUTS = [
+    (1, [1, 0, 0]),
+    (1, [0, 1, 0]),
+    (1, [0, 0, 1]),
+    (1, [1, 1, 1]),
+    (1, [0, 0, 0]),
+    (1, [0, 0, 0]),
+]
+
+OUTPUTS = {
+    rr: [
+        (1, does_not_raise),
+        (1. / 2, does_not_raise),
+        (1. / 3, does_not_raise),
+        (1., does_not_raise),
+        (0, does_not_raise),
+        (0, does_not_raise),
+    ],
+    recall: [
+        (True, does_not_raise),
+        (True, does_not_raise),
+        (True, does_not_raise),
+        (True, does_not_raise),
+        (False, does_not_raise),
+        (False, does_not_raise),
+    ],
+    precision: [
+        (1. / 3., does_not_raise),
+        (1. / 3., does_not_raise),
+        (1. / 3., does_not_raise),
+        (1. / 3., does_not_raise),
+        (False, does_not_raise),
+        (False, does_not_raise),
+    ],
+    ndcg: [
+        (1., does_not_raise),
+        (1. / np.log2(2 + 1), does_not_raise),  # idx + 1, (idx starts from 1)
+        (1. / np.log2(3 + 1), does_not_raise),  # idx + 1, (idx starts from 1)
+        (1., does_not_raise),
+        (np.nan, does_not_raise),
+        (np.nan, does_not_raise),
+    ],
+    ap: [
+        (1. / 3., does_not_raise),
+        (0., does_not_raise),
+        (0., does_not_raise),
+        (1. / 3., does_not_raise),
+        (False, does_not_raise),
+        (False, does_not_raise),
+    ],
+}
 
 
 @pytest.mark.parametrize("y_true, y_pred, output", [
