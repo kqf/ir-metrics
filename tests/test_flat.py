219aa9a86d79f7acd84828a0f635cf20e2647bbc
Sasha <q.dev.null@gmail.com>
2020-12-08 07:19:48 +0200

    Fix the flat ndcg modules

--- tests/test_flat.py
+++ tests/test_flat.py
@@ -37,3 +37,44 @@ def test_calculates_data(data, outputs, expected, measure):
         measure=measure,
     )
     np.testing.assert_almost_equal(out.values, outputs)
+
+
+"""
+    This is a separate test for NDCG as it might accept nonbinary judgements
+"""
+
+EXAMPLES = [
+    ((3, 2, 3, 0, 1, 2), 0.948810748),
+    ((1, 0, 0, 0, 0, 0), 1.0),
+    ((0, 1, 0, 0, 0, 0), 0.630929753),
+    ((1, 1, 0, 0, 0, 0), 1.0),
+    ((0, 0, 0, 0, 0, 1), 0.356207187),
+    ((0, 0, 0, 0, 0, 0), np.NaN)
+]
+
+
+@pytest.fixture
+def nonbinary_data():
+    queries, answers = zip(*EXAMPLES)
+    # Explode the examples
+    df = pd.DataFrame([
+        {
+            "query": i,
+            "relevance": rel,
+            "weights": 1,
+        }
+        for i, relevance in enumerate(queries)
+        for rel in relevance
+    ])
+    return df, answers
+
+
+def test_nonbinary_ndcg(nonbinary_data):
+    df, answers = nonbinary_data
+    outputs = flat(
+        df,
+        query_col="query",
+        relevance_col="relevance",
+        measure=ndcg,
+    )
+    np.testing.assert_almost_equal(outputs.values, answers)
