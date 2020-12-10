622930e7eff66a03f930a402c717b4a52109790f
Sasha <q.dev.null@gmail.com>
2020-12-10 08:24:49 +0200

    Add thes with multiple labels

--- tests/test_relevance.py
+++ tests/test_relevance.py
@@ -2,6 +2,7 @@ import pytest
 import numpy as np
 
 from irmetrics.relevance import unilabel, multilabel
+from contextlib import contextmanager
 
 
 def arr(x):
@@ -15,3 +16,24 @@ def arr(x):
 @pytest.mark.parametrize("relevance", [unilabel, multilabel])
 def test_single_labels(y_true, y_pred, output, relevance):
     np.testing.assert_equal(relevance(y_true, y_pred), output)
+
+
+@contextmanager
+def does_not_raise():
+    yield
+
+
+def conditional_raises(relevance):
+    if relevance == unilabel:
+        return pytest.raises(ValueError)
+    return does_not_raise()
+
+
+@pytest.mark.parametrize("y_true, y_pred, output", [
+    (arr([1, 2]), arr([1, 2, 3]), arr([True, True, False])),
+    (arr([1, 2]), arr([2, 2, 3]), arr([True, True, False])),
+])
+@pytest.mark.parametrize("relevance", [unilabel, multilabel])
+def test_multiple_labels(y_true, y_pred, output, relevance):
+    with conditional_raises(relevance):
+        np.testing.assert_equal(relevance(y_true, y_pred), output)
