02ad906bf03e64335a8da4262469f6f58f64f236
kqf <q.dev.null@gmail.com>
2020-12-10 08:05:23 +0200

    Fix coverage (#23)
    * Add possibility to handle exception for coverages
    
    * Fix the coverage tests
    
    * Add code-coverage reports to CI
    
    * Add the coverage badge

--- tests/test_coverage.py
+++ tests/test_coverage.py
@@ -1,7 +1,13 @@
 import pytest
 import numpy as np
 
+from contextlib import contextmanager
 from irmetrics.coverage import coverage, iou
+
+
+@contextmanager
+def does_not_raise():
+    yield
 
 
 @pytest.mark.parametrize("y_pred, output", [
@@ -23,17 +29,32 @@ def test_coverage(y_pred, output, n_samples=128):
     assert np.all(coverage(y_preds) == outputs)
 
 
-@pytest.mark.parametrize("y_true, y_pred, output", [
-    (1, [1, 2, 3], 1. / 3),
-    (1, [2, 1, 3], 1. / 3),
-    (1, [2, 3, 1], 1. / 3),
-    (1, [3, 4, 5], 0),
-])
-def test_iou(y_true, y_pred, output, n_samples=128):
-    assert iou(y_true, y_pred) == output
+@pytest.mark.parametrize(
+    "y_true, y_pred, output, exception",
+    [
+        (1, [1, 2, 3], 1. / 3, does_not_raise()),
+        (1, [2, 1, 3], 1. / 3, does_not_raise()),
+        (1, [2, 3, 1], 1. / 3, does_not_raise()),
+        (1, [3, 4, 5], 0, does_not_raise()),
+        (1, [1, 1, 1], 0, pytest.raises(ValueError)),
+        ([1, 1, 1], 1, 0, pytest.raises(ValueError)),
+    ])
+def test_iou(y_true, y_pred, output, exception, n_samples=128):
+    with exception:
+        assert iou(y_true, y_pred) == output
 
-    # Now the vectorized output
+
+@pytest.mark.parametrize("y_true, y_pred, output, exception", [
+    (1, [1, 2, 3], 1. / 3, does_not_raise()),
+    (1, [2, 1, 3], 1. / 3, does_not_raise()),
+    (1, [2, 3, 1], 1. / 3, does_not_raise()),
+    (1, [3, 4, 5], 0, does_not_raise()),
+    (1, [1, 1, 1], 0, pytest.raises(ValueError)),
+    ([1, 1, 1], 1, 0, pytest.raises(ValueError)),
+])
+def test_iou_vectorized(y_true, y_pred, output, exception, n_samples=128):
     y_trues = np.repeat(np.array(y_true), n_samples)
     y_preds = np.repeat(np.atleast_2d(y_pred), n_samples, axis=0)
     outputs = np.repeat(np.array(output), n_samples)
-    assert np.all(iou(y_trues, y_preds) == outputs)
+    with exception:
+        np.testing.assert_array_equal(iou(y_trues, y_preds), outputs)
