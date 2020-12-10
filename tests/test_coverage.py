f1bc5578a4b46cfac18cca18868013b4484150b9
Sasha <q.dev.null@gmail.com>
2020-12-10 07:48:23 +0200

    Fix the coverage tests

--- tests/test_coverage.py
+++ tests/test_coverage.py
@@ -29,19 +29,32 @@ def test_coverage(y_pred, output, n_samples=128):
     assert np.all(coverage(y_preds) == outputs)
 
 
-@pytest.mark.parametrize("y_true, y_pred, output, exception", [
-    (1, [1, 2, 3], 1. / 3, does_not_raise),
-    (1, [2, 1, 3], 1. / 3, does_not_raise),
-    (1, [2, 3, 1], 1. / 3, does_not_raise),
-    (1, [3, 4, 5], 0, does_not_raise),
-])
+@pytest.mark.parametrize(
+    "y_true, y_pred, output, exception",
+    [
+        (1, [1, 2, 3], 1. / 3, does_not_raise()),
+        (1, [2, 1, 3], 1. / 3, does_not_raise()),
+        (1, [2, 3, 1], 1. / 3, does_not_raise()),
+        (1, [3, 4, 5], 0, does_not_raise()),
+        (1, [1, 1, 1], 0, pytest.raises(ValueError)),
+        ([1, 1, 1], 1, 0, pytest.raises(ValueError)),
+    ])
 def test_iou(y_true, y_pred, output, exception, n_samples=128):
-    with exception():
+    with exception:
         assert iou(y_true, y_pred) == output
 
-    # Now the vectorized output
+
+@pytest.mark.parametrize("y_true, y_pred, output, exception", [
+    (1, [1, 2, 3], 1. / 3, does_not_raise()),
+    (1, [2, 1, 3], 1. / 3, does_not_raise()),
+    (1, [2, 3, 1], 1. / 3, does_not_raise()),
+    (1, [3, 4, 5], 0, does_not_raise()),
+    (1, [1, 1, 1], 0, pytest.raises(ValueError)),
+    ([1, 1, 1], 1, 0, pytest.raises(ValueError)),
+])
+def test_iou_vectorized(y_true, y_pred, output, exception, n_samples=128):
     y_trues = np.repeat(np.array(y_true), n_samples)
     y_preds = np.repeat(np.atleast_2d(y_pred), n_samples, axis=0)
     outputs = np.repeat(np.array(output), n_samples)
-    with exception():
-        assert np.all(iou(y_trues, y_preds) == outputs)
+    with exception:
+        np.testing.assert_array_equal(iou(y_trues, y_preds), outputs)
