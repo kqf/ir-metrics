1125e1bb08f86b83611e4c92706e94cdd6aed20f
Sasha <q.dev.null@gmail.com>
2020-12-17 07:25:00 +0200

    Add classifiers

--- setup.py
+++ setup.py
@@ -4,23 +4,32 @@ from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 try:
-    README = open(os.path.join(here, 'README.rst')).read()
+    README = open(os.path.join(here, "README.rst")).read()
 except IOError:
-    README = ''
+    README = ""
 
 setup(
     name="ir-metrics",
     use_scm_version=True,
-    description='The most common information retrieval (IR) metrics',
+    description="The most common information retrieval (IR) metrics",
     long_description=README,
     url="https://github.com/kqf/ir-metrics",
-    setup_requires=['setuptools_scm'],
+    setup_requires=["setuptools_scm"],
     packages=find_packages(),
+    classifiers=[  # Optional
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
     install_requires=[
         "numpy",
     ],
     extras_require={
-        # Didn't come up with a better name
-        'pandas': ["pandas"],
+        # Didn"t come up with a better name
+        "pandas": ["pandas"],
     },
 )
